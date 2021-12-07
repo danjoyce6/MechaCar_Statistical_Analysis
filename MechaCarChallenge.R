454304a4a214c75a6b8df9b8d7cb3891a692f7a5
danjoyce6 <danieljoyce@usf.edu>
2021-12-07 16:22:07 -0500

    lot summary

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,10 @@ mpg_model <- lm(mpg ~ vehicle_length + vehicle_weight + ground_clearance +
     AWD, data=mecha_data) #create a multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + ground_clearance + 
     AWD, data=mecha_data)) #generate summary data and statistics for the model
+
+coil_data <- read.csv('Suspension_Coil.csv') #import data
+total_summary <- coil_data %>% summarize(Mean=mean(PSI), Variance=var(PSI), 
+    SD=sd(PSI), Median=median(PSI))
+lot_table <- coil_data %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean=mean(PSI), Variance=var(PSI),
+  SD=sd(PSI), Median=median(PSI))
