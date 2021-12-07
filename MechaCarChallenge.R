819fa1885fd10a6aab4d61508a09ef34732bda99
danjoyce6 <danieljoyce@usf.edu>
2021-12-07 14:40:00 -0500

    multiple linear regression

---
+++ MechaCarChallenge.R
@@ -0,0 +1,7 @@
+mecha_data <- read.csv('MechaCar_mpg.csv') #import data
+head(mecha_data)
+library(dplyr) # add in the dplyr library
+mpg_model <- lm(mpg ~ vehicle_length + vehicle_weight + ground_clearance +
+    AWD, data=mecha_data) #create a multiple linear regression model
+summary(lm(mpg ~ vehicle_length + vehicle_weight + ground_clearance + 
+    AWD, data=mecha_data)) #generate summary data and statistics for the model
